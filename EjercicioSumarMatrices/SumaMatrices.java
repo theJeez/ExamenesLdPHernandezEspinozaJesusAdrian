8c314e14f9b4651bb40b0076e9f3f3987536c9f1
theJeez <he525169@uaeh.edu.mx>
2025-09-04 21:41:51 -0600

    Create SumaMatrices.java

---
+++ EjercicioSumarMatrices/SumaMatrices.java
@@ -0,0 +1,54 @@
+public class SumaMatrices
+{
+	public static void main(String[] args) {
+        
+        int[][] matriz1 = new int[4][4];
+        int[][] matriz2 = new int[4][4];
+        int[][] matrizRes = new int[4][4];
+        int i,j;
+        
+        for(i = 0;i < 4;i++){
+            for(j = 0;j < 4;j++){
+                matriz1[i][j] = j;
+                matriz2[i][j] = j;
+            }
+        }
+        
+        System.out.println("Matriz 1");
+        for(i = 0;i < 4;i++){
+            for(j = 0;j < 4;j++){
+                System.out.print(matriz1[i][j]);
+            }
+            System.out.println("");
+        }
+        
+        System.out.println("Matriz 2");
+        for(i = 0;i < 4;i++){
+            for(j = 0;j < 4;j++){
+                System.out.print(matriz2[i][j]);
+            }
+            System.out.println("");
+        }
+        
+        for(i = 0;i < 4;i++){
+            for(j = 0;j < 4;j++){
+                matrizRes[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+        
+        System.out.println("Matriz Resultado");
+        for(i = 0;i < 4;i++){
+            for(j = 0;j < 4;j++){
+                System.out.print(matrizRes[i][j]);
+            }
+            System.out.println("");
+        }
+        
+	}
+}
+
+
+
+
+
+

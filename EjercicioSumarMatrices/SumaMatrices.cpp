48bfda150c9fa5a2bd1f50cc21133d4621db72f2
theJeez <he525169@uaeh.edu.mx>
2025-09-04 20:15:06 -0600

    Create SumaMatrices.cpp

---
+++ EjercicioSumarMatrices/SumaMatrices.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    int matriz1[4][4], matriz2[4][4], matrizRes[4][4]; //matriz 1,2 y en la que se guardara el resultado
+    int i,j; //contadores para sumar
+
+    for (i = 0; i < 4; i++) { //asigna valor a las matrices
+        for (j = 0; j < 4; j++) {
+            matriz1[i][j] = j;
+            matriz2[i][j] = j;
+        }
+    }
+
+    std::cout << "Matriz 1: " << std::endl;
+    for (i = 0; i < 4; i++) { //imprime la matriz 1
+        for (j = 0; j < 4; j++) {
+            std::cout << matriz1[i][j] << " "; 
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Matriz 2: " << std::endl;
+    for (i = 0; i < 4; i++) { //imprime la matriz 2
+        for (j = 0; j < 4; j++) {
+            std::cout << matriz2[i][j] << " "; 
+        }
+        std::cout << std::endl;
+    }
+    
+    for (i = 0; i < 4; i++) { //suma las dos matrices
+        for (j = 0; j < 4; j++) {
+            matrizRes[i][j] = matriz1[i][j] + matriz2[i][j]; 
+        }
+    }
+
+    std::cout << "Matriz Resultado: " << std::endl;
+    for (i = 0; i < 4; i++) { //imprime la matriz resultado
+        for (j = 0; j < 4; j++) {
+            std::cout << matrizRes[i][j] << " "; 
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
